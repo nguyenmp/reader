7e93aedd5452160e821f8e5a95f347a2d4fd4d9d
Mark Nguyen <mpnguyen@umail.ucsb.edu>
2014-07-17 21:15:30 -0700

    Added reddit as a submodule

---
+++ reddit/src/main/java/com/nguyenmp/reddit/Reddit.java
@@ -0,0 +1,10 @@
+package com.nguyenmp.reddit;
+
+public class Reddit {
+
+    /** */
+    public Reddit() throws NoSuchMethodException {
+        super();
+        throw new NoSuchMethodException("This constructor has not been implemented yet");
+    }
+}
