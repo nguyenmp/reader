65e64be24e54b0a9fd8878b04b1774d230661d69
Mark Nguyen <mpnguyen@umail.ucsb.edu>
2014-08-03 14:16:15 -0700

    Removed unused Replies class

--- reddit/src/main/java/com/nguyenmp/reddit/data/Replies.java
+++
@@ -1,9 +0,0 @@
-package com.nguyenmp.reddit.data;
-
-import org.codehaus.jackson.map.annotate.JsonDeserialize;
-import org.codehaus.jackson.map.annotate.JsonSerialize;
-
-@JsonDeserialize(using = RepliesDeserializer.class)
-@JsonSerialize
-public class Replies extends Listing<Reply> {
-}
